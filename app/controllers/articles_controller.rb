e8f70562098d5e7b14c1082f93dc268b4c1f6f58
Alex Morais <alexsandermorais@hotmail.com>
2022-11-08 15:11:08 -0300

    feat: creating article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,24 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new # (**)ESSA ACTION
+  end
+
+  def create
+    @article = Article.new(article_params) # quando clicar no create da tela, ira jogar os parametros e verificar se retornou true // (com o render os parametros do article aqui continuarao na new (nao serao perdidos) )
+
+    if @article.save # se retornou true redireciona para @article <- eh um article que foi criado
+      redirect_to @article
+    else
+      render :new # new eh a action de cima (ver (**)) (o render eh utilizado pq se for utilizado o redirect, tudo que foi digitado sera perdido)
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body) # da um request em todos os parametros de article e permite apenas title e body
+  end
 end
